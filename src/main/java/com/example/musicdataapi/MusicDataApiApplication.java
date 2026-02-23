67facfcc0d3961962f024d46aa36bface61d0279
ice_jmendes <ice_jmendes@uolinc.com>
2026-02-23 09:45:24 -0300

    commit inicial

---
+++ src/main/java/com/example/musicdataapi/MusicDataApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.musicdataapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicDataApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicDataApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/musicdataapi/MusicDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.musicdataapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicDataApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
